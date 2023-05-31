8bb965a923266c565894ed88d2570f55a9fe9b9b
Gonzalo Zucca <gonzalozucca_4@outlook.com>
2023-05-31 13:54:38 +0200

    dialog box

---
+++ star-wars-client/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card__background">Hello!</div>
+  <div>{{ characters }}</div>
+</template>
+
+<script setup>
+  defineProps(["characters"]);
+
+</script>
+
+<style scoped>
+  .card__background {
+    background-color: rgba(184, 237, 255, 0.1);
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    color: #9effff;
+    border: 0;
+  }
+
+  @media screen and (min-width: 900px) {
+  }
+</style>
--- star-wars-client/pages/index.vue
+++ star-wars-client/pages/index.vue
@@ -1,4 +1,7 @@
 <template>
+  <dialog id="characterDialog">
+    <Card v-bind="characters" />
+  </dialog>
   <div class="home">
     <div class="home__scroll">
       <header class="container">
@@ -8,8 +11,15 @@
         <section class="main__col">
           <h2 class="main__col__title">Characters</h2>
           <div class="main__col__list">
-            <div v-for="c in characters">
-              <h3>{{ c.name }}</h3>
+            <div v-bind="characters" v-for="character in characters">
+              <h3
+                :birthYear="character.birth_year"
+                v-bind="character"
+                @click="(event) => handleClick(event)"
+                class="main__col__list__item"
+              >
+                {{ character.name }}
+              </h3>
             </div>
           </div>
         </section>
@@ -17,7 +27,7 @@
           <h2 class="main__col__title">Planets</h2>
           <div class="main__col__list">
             <div v-for="p in planets">
-              <h3>{{ p.name }}</h3>
+              <h3 class="main__col__list__item">{{ p.name }}</h3>
             </div>
           </div>
         </section>
@@ -25,7 +35,7 @@
           <h2 class="main__col__title">Films</h2>
           <div class="main__col__list">
             <div v-for="f in films">
-              <h3>{{ f.title }}</h3>
+              <h3 class="main__col__list__item">{{ f.title }}</h3>
             </div>
           </div>
         </section>
@@ -39,10 +49,11 @@
 </template>
 
 <script setup>
+  import { ref, provide } from "vue";
   const { data: charactersResponse } = await useFetch(
     "http://34.125.250.75:8000/characters"
   );
-  const characters = charactersResponse.value.data;
+  const characters = ref(charactersResponse.value.data);
 
   const { data: planetsResponse } = await useFetch(
     "http://34.125.250.75:8000/planets"
@@ -53,9 +64,29 @@
     "http://34.125.250.75:8000/films"
   );
   const films = filmsResponse.value.data;
+
+  function handleClick(event) {
+    console.log(event);
+    characterDialog.showModal();
+  }
+
+  const setOpened = ref(false);
 </script>
 
 <style scoped>
+  #characterDialog {
+    height: 60vh;
+    width: 70vw;
+    padding: 0;
+    border: solid 5px rgba(127, 222, 254, 0.875);
+    background-color: rgba(255, 255, 255, 0);
+    border-radius: 15px;
+  }
+
+  #characterDialog::backdrop {
+    background-color: rgba(0, 0, 0, 0.733);
+  }
+
   .background {
     width: 100%;
     z-index: 1;
@@ -102,24 +133,42 @@
     display: flex;
     flex-direction: column;
     text-align: center;
-    background-color: purple;
+    background: rgba(184, 237, 255, 0.05);
+    border: solid 3px rgba(127, 222, 254, 0.05);
+    border-radius: 15px;
   }
 
   .main__col {
     width: 60%;
-    background-color: blue;
   }
 
   .main__col__title {
     font-size: clamp(1.75rem, 1.5227rem + 0.9697vw, 2.25rem);
     font-weight: 700;
-    margin: .3em 0em;
+    margin: 0.3em 0em;
+    color: #9effff;
+    cursor: default;
   }
 
   .main__col__list {
     overflow-y: auto;
     max-height: 60vh;
-    background-color: red;
+  }
+
+  .main__col__list__item {
+    color: #9effff;
+    background: rgba(184, 237, 255, 0.1);
+    border: solid 1px #9effff;
+    border-radius: 5px;
+    padding: 6%;
+    margin: 6%;
+    transform: rotateX(30deg);
+    cursor: pointer;
+  }
+
+  .main__col__list__item:hover {
+    color: #c0f8f8;
+    background: rgba(184, 237, 255, 0.568);
   }
 
   @media screen and (min-width: 900px) {
@@ -133,6 +182,7 @@
       flex-direction: row;
       justify-content: space-between;
       align-items: start;
+      animation: float 6s infinite alternate;
     }
   }
 </style>
