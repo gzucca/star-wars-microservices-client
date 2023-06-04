6dc6c29f73b563ed52bbbe56fd5e349fc373d3d6
Gonzalo Zucca <gonzalozucca_4@outlook.com>
2023-06-04 17:16:23 +0200

    deleted folder

---
+++ components/PlanetCard.vue
@@ -0,0 +1,113 @@
+<template>
+  <section tabindex="0" @keyup.esc="props.closePopup" class="card__background">
+    <h1 class="card__title">{{ name }}</h1>
+    <div class="card__list">
+      <p class="card__item">Rotation period: {{ rotation_period }}</p>
+      <p class="card__item">Orbital period: {{ orbital_period }}</p>
+      <p class="card__item">Diameter: {{ diameter }}</p>
+      <p class="card__item">Climate: {{ climate }}</p>
+      <p class="card__item">Gravity: {{ gravity }}</p>
+      <p class="card__item">Terrain: {{ terrain }}</p>
+      <p class="card__item">Surface water: {{ surface_water }}</p>
+    </div>
+    <div v-if="residents.length > 0">
+      <h2 class="card__subtitle">Residents:</h2>
+      <div class="card__list">
+        <p class="card__item" v-for="resident in residents">
+          {{ resident.name }} -
+        </p>
+      </div>
+    </div>
+    <div v-if="films.length > 0">
+      <h2 class="card__subtitle">Films:</h2>
+      <div class="card__list">
+        <p v-for="film in films" class="card__item">{{ film.title }} -</p>
+      </div>
+    </div>
+    <button class="card__button" @click="props.closePopup">CLOSE</button>
+  </section>
+</template>
+
+<script setup>
+  const props = defineProps([
+    "_id",
+    "name",
+    "rotation_period",
+    "orbital_period",
+    "diameter",
+    "climate",
+    "gravity",
+    "terrain",
+    "surface_water",
+    "residents",
+    "films",
+    "closePopup",
+  ]);
+</script>
+
+<style scoped>
+  .card__background {
+    background: rgba(184, 237, 255, 0.1);
+    color: #9effff;
+    border: solid 3px rgba(127, 222, 254, 0.4);
+    position: relative;
+    height: fit-content;
+    width: 60vw;
+    border-radius: 10px;
+    padding: 1rem;
+    margin: 0;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    max-height: 100%;
+    overflow-y: scroll;
+  }
+
+  .card__title {
+    font-size: x-large;
+    margin-block: 1rem;
+  }
+
+  .card__list {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.5rem;
+  }
+
+  .card__item {
+    font-weight: 400;
+    font-size: medium;
+  }
+
+  .card__subtitle {
+    margin-block: 1rem;
+    font-size: larger;
+  }
+
+  .card__button {
+    color: #9effff;
+    background: rgba(184, 237, 255, 0.1);
+    border: solid 1px #9effff;
+    border-radius: 5px;
+    padding: 0.8em 1em;
+    cursor: pointer;
+    margin-block: 1rem;
+    font-size: large;
+    font-weight: 600;
+  }
+
+  @media screen and (min-width: 900px) {
+    .card__button,
+    .card__item {
+      font-size: large;
+    }
+    .card__title {
+      font-size: xx-large;
+    }
+
+    .card__subtitle {
+      font-size: x-large;
+    }
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,251 @@
+<template>
+  <div v-if="state.showCharacter" class="home__card">
+    <CharacterCard v-bind="state.character" :closePopup="closePopup" />
+  </div>
+  <div v-if="state.showFilm" class="home__card">
+    <FilmCard v-bind="state.film" :closePopup="closePopup" />
+  </div>
+  <div v-if="state.showPlanet" class="home__card">
+    <PlanetCard v-bind="state.planet" :closePopup="closePopup" />
+  </div>
+  <div class="home">
+    <div class="home__scroll">
+      <header class="container">
+        <h1 class="home__title">Star Wars Microservices</h1>
+      </header>
+      <main class="container home__body">
+        <section class="home__col">
+          <h2 class="home__col__title">Characters</h2>
+          <div class="home__col__list">
+            <div v-bind="characters" v-for="character in characters">
+              <h3
+                tabindex="0"
+                @keyup.esc="closePopup"
+                @click="(event) => handleClickCharacter(event)"
+                :_id="character._id"
+                class="home__col__item"
+              >
+                {{ character.name }}
+              </h3>
+            </div>
+          </div>
+        </section>
+        <section class="home__col">
+          <h2 class="home__col__title">Planets</h2>
+          <div class="home__col__list">
+            <div v-for="planet in planets">
+              <h3
+                tabindex="0"
+                @keyup.esc="closePopup"
+                @click="(event) => handleClickPlanet(event)"
+                :_id="planet._id"
+                class="home__col__item"
+              >
+                {{ planet.name }}
+              </h3>
+            </div>
+          </div>
+        </section>
+        <section class="home__col">
+          <h2 class="home__col__title">Films</h2>
+          <div class="home__col__list">
+            <div v-for="film in films">
+              <h3
+                tabindex="0"
+                @keyup.esc="closePopup"
+                @click="(event) => handleClickFilm(event)"
+                :_id="film._id"
+                class="home__col__item"
+              >
+                {{ film.title }}
+              </h3>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer class="home__footer">
+        <h4>
+          Built with <img class="home__logo" src="@/assets/logos/vuejs.svg" /> Vue.js and <img class="home__logo"  src="@/assets/logos/google_cloud.svg" /> Google
+          Cloud Platform
+        </h4>
+      </footer>
+    </div>
+  </div>
+  <div class="stars"></div>
+  <div class="twinkling"></div>
+  <div class="clouds"></div>
+  <nuxt-img src="/client-bg-2.png" class="home__background-img" />
+</template>
+
+<script setup>
+  import { ref } from "vue";
+
+  const state = ref({
+    showCharacter: false,
+    showFilm: false,
+    showPlanet: false,
+    character: {},
+    film: {},
+    planet: {},
+  });
+
+  const { data: charactersResponse } = await useFetch(
+    "http://34.125.250.75:8000/characters"
+  );
+  const characters = ref(charactersResponse.value.data);
+
+  const { data: planetsResponse } = await useFetch(
+    "http://34.125.250.75:8000/planets"
+  );
+  const planets = ref(planetsResponse.value.data);
+
+  const { data: filmsResponse } = await useFetch(
+    "http://34.125.250.75:8000/films"
+  );
+  const films = ref(filmsResponse.value.data);
+
+  function handleClickCharacter(event) {
+    state.value.character = characters.value.find(
+      (character) => character._id === event.target.attributes._id.value
+    );
+    state.value.showCharacter = !state.value.showCharacter;
+  }
+
+  function handleClickPlanet(event) {
+    state.value.planet = planets.value.find(
+      (planet) => planet._id === event.target.attributes._id.value
+    );
+    state.value.showPlanet = !state.value.showPlanet;
+  }
+
+  function handleClickFilm(event) {
+    state.value.film = films.value.find(
+      (film) => film._id === event.target.attributes._id.value
+    );
+    state.value.showFilm = !state.value.showFilm;
+  }
+
+  function closePopup() {
+    state.value.showCharacter = false;
+    state.value.showFilm = false;
+    state.value.showPlanet = false;
+  }
+</script>
+
+<style scoped>
+  .home__card {
+    height: 100vh;
+    width: 100vw;
+    background-color: rgba(0, 0, 0, 0.9);
+    z-index: 100;
+    position: relative;
+  }
+
+  .home__background-img {
+    width: 100%;
+    z-index: 1;
+    bottom: 0;
+    position: absolute;
+    opacity: 0;
+    overflow: hidden;
+    object-fit: cover;
+  }
+  .home {
+    position: absolute;
+    display: block;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100vh;
+    z-index: 10;
+    overflow: hidden;
+  }
+
+  .home__scroll {
+    overflow-y: scroll;
+    height: 100%;
+    overflow-x: hidden;
+  }
+
+  .home__title {
+    font-family: "Starjhol", sans-serif;
+    color: rgb(255, 232, 31);
+    font-size: clamp(2rem, 1.4886rem + 2.1818vw, 3.125rem);
+    text-align: center;
+  }
+
+  .container {
+    max-width: 900px;
+    width: 100%;
+    margin-inline: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .home__body {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    background: rgba(184, 237, 255, 0.05);
+    border: solid 3px rgba(127, 222, 254, 0.05);
+    border-radius: 15px;
+  }
+
+  .home__col {
+    width: 60%;
+  }
+
+  .home__col__title {
+    font-size: clamp(1.75rem, 1.5227rem + 0.9697vw, 2.25rem);
+    font-weight: 700;
+    margin: 0.3em 0em;
+    color: #9effff;
+    cursor: default;
+  }
+
+  .home__col__list {
+    overflow-y: auto;
+    max-height: 60vh;
+  }
+
+  .home__col__item {
+    color: #9effff;
+    background: rgba(184, 237, 255, 0.1);
+    border: solid 1px #9effff;
+    border-radius: 5px;
+    padding: 6%;
+    margin: 6%;
+    cursor: pointer;
+  }
+
+  .home__footer{
+    display: flex;
+    place-content: center;
+    font-size: small;
+  }
+
+  .home__logo{
+    height: 2rem;
+  }
+
+  .home__col__item:hover {
+    color: #c0f8f8;
+    background: rgba(184, 237, 255, 0.568);
+  }
+
+  @media screen and (min-width: 900px) {
+    .home__background-img {
+      opacity: 100;
+    }
+    .home__scroll {
+      overflow-y: auto;
+    }
+    .home__body {
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: start;
+      animation: float 6s infinite alternate;
+    }
+  }
+</style>
